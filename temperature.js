70ea9e5ab85b3cd19eaed1842684ff9136501b47
Adele-Roos <121452950+Adele-Roos@users.noreply.github.com>
2023-02-24 09:43:36 +0200

    Variable names changed to lowerCamelCase
    The variables in the code had underscores in them in many instances, all these were changes to lowerCamelCase to conform with naming convensions.

--- temperature.js
+++ temperature.js
@@ -6,56 +6,56 @@ F - (Fahrenheit)
 K - (Kelvin)`);
 
 // This converts the string value entered to an uppercase letter
-let metric_uppercase = choosemetric.toUpperCase();
+let metricUppercase = choosemetric.toUpperCase();
 
 // User then inputs the temperature they want to convert
 let temperature = prompt("Enter the temperature that you want to convert: ");
 
 // This converts the string value into an integer
-let num_temperature = Number(temperature);
+let numTemperature = Number(temperature);
 
 // Next it prompts user for the metric they converting to
-let next_metric = prompt(`To which metric would you like to convert the temperature to?"
+let nextMetric = prompt(`To which metric would you like to convert the temperature to?"
 C - (Celsius)
 F - (Fahrenheit)
 K - (Kelvin)`);
 
 // This converts the string value entered to an uppercase letter
-let convert_to = next_metric.toLocaleUpperCase();
+let convertTo = nextMetric.toLocaleUpperCase();
 
-switch (metric_uppercase) {
+switch (metricUppercase) {
     case "C":
-        switch (convert_to) {
+        switch (convertTo) {
             case "F":
                 // Input temperature is converted from Celsius to Fahrenheit
-                let fahrenheit = (num_temperature * 9 / 5 + 32);
-                let fahrenheit_decimal = fahrenheit.toFixed(2);
-                console.log((num_temperature + "°C") + " is " + (fahrenheit_decimal + "°F"));
+                let fahrenheit = (numTemperature * 9 / 5 + 32);
+                let fahrenheitDecimal = fahrenheit.toFixed(2);
+                console.log((numTemperature + "°C") + " is " + (fahrenheitDecimal + "°F"));
                 break;
 
             case "K":
                 // Input temperature is converted from Celsius to Kelvin
-                let kelvin = ((num_temperature) + 273.15);
-                let kelvin_decimal = kelvin.toFixed(2);
-                console.log((num_temperature + "°C") + " is " + (kelvin_decimal + "K"));
+                let kelvin = ((numTemperature) + 273.15);
+                let kelvinDecimal = kelvin.toFixed(2);
+                console.log((numTemperature + "°C") + " is " + (kelvinDecimal + "K"));
                 break;
                 console.log("Invalid metic to convert to");
             }
             break;
 
     case "F":
-        switch (convert_to) {
+        switch (convertTo) {
             case "C":
                 // Input temperature is converted from Fahrenheit to Celsius
-                let celsius = ((num_temperature - 32) * 5 / 9);
-                let celsius_decimal = celsius.toFixed(2);
-                console.log((num_temperature + "°F") + " is " + (celsius_decimal + "°C"));
+                let celsius = ((numTemperature - 32) * 5 / 9);
+                let celsiusDecimal = celsius.toFixed(2);
+                console.log((numTemperature + "°F") + " is " + (celsiusDecimal + "°C"));
                 break;
             case "K":
                 // Input temperature is converted from Fahrenheit to Kelvin
-                let kelvin = ((num_temperature + 459.67) * 5 / 9);
-                let kelvin_decimal = kelvin.toFixed(2);
-                console.log((num_temperature + "°F") + " is " + (kelvin_decimal + "K"));
+                let kelvin = ((numTemperature + 459.67) * 5 / 9);
+                let kelvinDecimal = kelvin.toFixed(2);
+                console.log((numTemperature + "°F") + " is " + (kelvinDecimal + "K"));
                 break;
             default:
                 console.log("Invalid metic to convert to");
@@ -63,18 +63,18 @@ switch (metric_uppercase) {
         break;
 
     case "K":
-        switch (convert_to) {
+        switch (convertTo) {
             case "C":
                 // Input temperature is converted from Kelvin to Celsius
-                let celsius = ((num_temperature) - 273.15);
-                let celsius_decimal = celsius.toFixed(2);
-                console.log((num_temperature + "K") + " is " + (celsius_decimal + "°C"));
+                let celsius = ((numTemperature) - 273.15);
+                let celsiusDecimal = celsius.toFixed(2);
+                console.log((numTemperature + "K") + " is " + (celsiusDecimal + "°C"));
                 break;
             case "F":
                 // Input temperature is converted from Kelvin to Fahrenheit
-                let fahrenheit = ((num_temperature) * 9 / 5 - 459.67);
-                let fahrenheit_decimal = fahrenheit.toFixed(2);
-                console.log((num_temperature + "K") + " is " + (fahrenheit_decimal + "°F"));
+                let fahrenheit = ((numTemperature) * 9 / 5 - 459.67);
+                let fahrenheitDecimal = fahrenheit.toFixed(2);
+                console.log((numTemperature + "K") + " is " + (fahrenheitDecimal + "°F"));
                 break;
             default:
                 console.log("Invalid metic to convert to");
